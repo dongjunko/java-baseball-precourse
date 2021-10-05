6e1dcff13b3f464aacd41e6241224768b92dc41b
rhehdwns87 <rhehdwns87@gmail.com>
2021-10-05 22:59:14 +0900

    상수값 정의

---
+++ src/main/java/baseball/constants/Constants.java
@@ -0,0 +1,11 @@
+package baseball.constants;
+
+public class Constants {
+    public static final int DIGIT_SIZE = 3;
+    public static final String INPUT_NEWGAME = "1";
+    public static final String INPUT_EXIT = "2";
+    public static final String GAME_RESULT_STRIKE = "스트라이크";
+    public static final String GAME_RESULT_BALL = "볼";
+    public static final String GAME_RESULT_NOTHING = "낫싱";
+    public static final String ERROR_MESSAGE_BADINPUT = "올바른 형식이 아닙니다.";
+}
